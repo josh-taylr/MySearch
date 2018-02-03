f49a754132525e78dfe21dc85ba789051be5a633
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-02-04 01:54:58 +1300

    Match tags in order

--- src/test/kotlin/ParseTest.kt
+++ src/test/kotlin/ParseTest.kt
@@ -1,5 +1,7 @@
+import com.nhaarman.mockito_kotlin.inOrder
 import com.nhaarman.mockito_kotlin.mock
 import org.junit.Test
+import org.mockito.InOrder
 import org.mockito.Mockito.verify
 import java.io.File
 
@@ -27,5 +29,21 @@ class ParseTest {
         parser.parse(file)
         //then
         verify(index).endTag("DOC")
+    }
+
+    @Test
+    fun `match nested tags`() {
+        //given
+        val index = mock<Index>()
+        val parser = Parse(index)
+        val inOrder: InOrder = inOrder(index)
+        //when
+        val file = File("src/test/kotlin/single_document.xml")
+        parser.parse(file)
+        //then
+        inOrder.verify(index).startTag("DOC")
+        inOrder.verify(index).startTag("DOCNO")
+        inOrder.verify(index).endTag("DOCNO")
+        inOrder.verify(index).endTag("DOC")
     }
 }
