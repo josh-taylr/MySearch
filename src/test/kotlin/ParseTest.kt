18558034990182f9c997ddc4fef505014556c7f2
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-02-04 02:18:57 +1300

    Remove redundant tests

--- src/test/kotlin/ParseTest.kt
+++ src/test/kotlin/ParseTest.kt
@@ -2,34 +2,9 @@ import com.nhaarman.mockito_kotlin.inOrder
 import com.nhaarman.mockito_kotlin.mock
 import org.junit.Test
 import org.mockito.InOrder
-import org.mockito.Mockito.verify
 import java.io.File
 
 class ParseTest {
-
-    @Test
-    fun `match start tag`() {
-        //given
-        val index = mock<Index>()
-        val parser = Parse(index)
-        //when
-        val file = File("src/test/kotlin/single_document.xml")
-        parser.parse(file)
-        //then
-        verify(index).startTag("DOC")
-    }
-
-    @Test
-    fun `match end tag`() {
-        //given
-        val index = mock<Index>()
-        val parser = Parse(index)
-        //when
-        val file = File("src/test/kotlin/single_document.xml")
-        parser.parse(file)
-        //then
-        verify(index).endTag("DOC")
-    }
 
     @Test
     fun `match nested tags`() {
