afc6c8a69eaa662c7e37d967f32f20bd4b749654
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-02-04 13:05:57 +1300

    Remove string buffer from parser

--- src/main/kotlin/Parse.kt
+++ src/main/kotlin/Parse.kt
@@ -1,24 +1,17 @@
 import java.io.File
 
-const val EOF = -1
-
 class Parse constructor(private val index: Index) {
 
     fun parse(file: File) {
         val regex = Regex("<([a-zA-Z][a-zA-Z0-9]*)>|</([a-zA-Z][a-zA-Z0-9]*)>")
         file.bufferedReader().use {
-            val buffer = StringBuffer()
-            while (true) {
-                val c = it.read()
-                if (c == EOF) break
-                buffer.append(c.toChar())
-                val result = regex.find(buffer)
-                if (null != result) {
-                    val (startTag, endTag) = result.destructured
-                    if (startTag.isNotEmpty()) index.startTag(startTag)
-                    if (endTag.isNotEmpty()) index.endTag(endTag)
-                    buffer.setLength(0)
-                }
+            val document = it.readText()
+            var result = regex.find(document)
+            while (null != result) {
+                val (startTag, endTag) = result.destructured
+                if (startTag.isNotEmpty()) index.startTag(startTag)
+                if (endTag.isNotEmpty()) index.endTag(endTag)
+                result = result.next()
             }
         }
     }
