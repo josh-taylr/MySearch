8e782ddeb982dc00c5b25cb2c5425a9353d437d8
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-02-05 19:27:16 +1300

    Tidy test class

--- src/test/kotlin/ParseTest.kt
+++ src/test/kotlin/ParseTest.kt
@@ -1,20 +1,27 @@
 import com.nhaarman.mockito_kotlin.inOrder
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import org.junit.Before
 import org.junit.Test
-import org.mockito.InOrder
 import java.io.File
 
 class ParseTest {
 
+    private val file = File("src/test/kotlin/single_document.xml")
+    private val index = mock<Index>()
+    private val inOrder = inOrder(index)
+
+    lateinit var parser: Parse
+
+    @Before
+    fun setUp() {
+        //given
+        parser = Parse(index)
+    }
+
     @Test
     fun `match nested tags`() {
-        //given
-        val index = mock<Index>()
-        val parser = Parse(index)
-        val inOrder: InOrder = inOrder(index)
         //when
-        val file = File("src/test/kotlin/single_document.xml")
         parser.parse(file)
         //then
         inOrder.verify(index).startTag("DOC")
@@ -25,12 +32,7 @@ class ParseTest {
 
     @Test
     fun `match word in correct tags`() {
-        //given
-        val index = mock<Index>()
-        val parser = Parse(index)
-        val inOrder: InOrder = inOrder(index)
         //when
-        val file = File("src/test/kotlin/single_document.xml")
         parser.parse(file)
         //then
         inOrder.verify(index).startTag("DOCNO")
@@ -40,11 +42,7 @@ class ParseTest {
 
     @Test
     fun `convert escape characters`() {
-        //given
-        val index = mock<Index>()
-        val parser = Parse(index)
         //when
-        val file = File("src/test/kotlin/single_document.xml")
         parser.parse(file)
         //then
         verify(index).word("<1>")
@@ -52,29 +50,17 @@ class ParseTest {
     }
 
     @Test
-    @Throws(Exception::class)
     fun `parser notifies indexer before parsing begins`() {
-        //given
-        val index = mock<Index>()
-        val parse = Parse(index)
-        val inOrder = inOrder(index)
         //when
-        val file = File("src/test/kotlin/single_document.xml")
-        parse.parse(file)
+        parser.parse(file)
         //then
         inOrder.verify(index).beginIndexing()
     }
 
     @Test
-    @Throws(Exception::class)
     fun `parser notifies indexer after parsing ends`() {
-        //given
-        val index = mock<Index>()
-        val parse = Parse(index)
-        val inOrder = inOrder(index)
         //when
-        val file = File("src/test/kotlin/single_document.xml")
-        parse.parse(file)
+        parser.parse(file)
         //then
         inOrder.verify(index).endIndexing()
         inOrder.verifyNoMoreInteractions()
