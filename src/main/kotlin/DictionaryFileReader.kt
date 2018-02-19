bb36423541f5ed57b2dc0c0773bcc7fd6bf18ea0
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-02-20 00:51:11 +1300

    Implement dictionary writer

---
+++ src/main/kotlin/DictionaryFileReader.kt
@@ -0,0 +1,59 @@
+import java.io.File
+import java.io.RandomAccessFile
+import java.util.*
+
+class DictionaryFileReader {
+
+    fun readDictionary(file: File): Map<String, DictionaryBlock> {
+        val map = mutableMapOf<String, DictionaryBlock>()
+        RandomAccessFile(file, "r").run {
+            seek(0) // reset
+
+            val dictionaryPosition = readLong()
+            seek(dictionaryPosition)
+
+            while (true) {
+                val termLength = read()
+                if (EOF == termLength) break
+                val term = ByteArray(termLength).also(::readFully).let { String(it) }
+                val position = readLong()
+                val size = readLong()
+                map[term] = DictionaryBlock(position, size)
+            }
+        }
+        return map
+    }
+
+    fun readDictionary(file: File, block: DictionaryBlock): Map<String, PostingsBlock> {
+        val map = mutableMapOf<String, PostingsBlock>()
+        RandomAccessFile(file, "r").run {
+            seek(block.position)
+            while (filePointer < (block.position + block.size)) {
+                val termLength = read()
+                val term = ByteArray(termLength).also(::readFully).let { String(it) }
+                val position = readLong()
+                val size = readLong()
+                map[term] = PostingsBlock(position, size)
+            }
+        }
+        return map
+    }
+
+    fun readPostings(file: File, block: PostingsBlock): Set<DocumentNumber> {
+        val set = mutableSetOf<DocumentNumber>()
+        RandomAccessFile(file, "r").run {
+            seek(block.position)
+            while (filePointer < (block.position + block.size)) {
+                val value = readLong()
+                set.add(DocumentNumber(value))
+            }
+        }
+        return set
+    }
+
+    private fun <K, V> mutableMapOf() = TreeMap<K, V>()
+}
+
+data class DictionaryBlock(val position: Long, val size: Long)
+
+data class PostingsBlock(val position: Long, val size: Long)
--- src/main/kotlin/constants.kt
+++ src/main/kotlin/constants.kt
@@ -1,3 +1,5 @@
 const val BYTE_SIZE = 8
 const val INT_SIZE  = 32
 const val LONG_SIZE = 64
+
+const val EOF = -1
