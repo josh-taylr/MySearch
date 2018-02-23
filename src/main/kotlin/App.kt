cde7b47f2be0c533a37ecbd0944895d1f204a3c6
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-02-23 21:51:23 +1300

    Remove grammar checker

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,14 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.File
-import java.util.*
-import kotlin.collections.HashMap
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-    if (args.contains("--grammar")) {
-        Parse(GrammarIndex()).parse(File(WSJ_FILE))
-        return
-    }
 
     val dictionaryFile = File(INDEX_DIR + "/dictionary.dat")
 
@@ -36,34 +30,4 @@ fun main(args: Array<String>) {
                 .reduce {acc, postings -> acc.intersect(postings) }
                 .forEach(::println)
     }.let { println("Search complete in $it milliseconds.") }
-}
-
-private class GrammarIndex : Index {
-
-    val grammar = HashMap<String, MutableSet<String>>()
-    val tags = Stack<String>()
-
-    override fun beginIndexing() {
-        tags.push("Root")
-    }
-
-    override fun endIndexing() {
-        println(grammar)
-
-        grammar.clear()
-        tags.removeAllElements()
-    }
-
-    override fun startTag(tag: String) {
-        val children = grammar.getOrPut(tags.peek()) { mutableSetOf() }
-        children.add(tag)
-        tags.push(tag)
-    }
-
-    override fun endTag(tag: String) {
-        tags.pop()
-    }
-
-    override fun word(term: String) = Unit
-
 }
