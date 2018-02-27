0e297ed71b3696dff7eefa4eef153615043de6db
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-02-27 19:59:40 +1300

    Rename typealias

--- src/main/kotlin/Dictionary.kt
+++ src/main/kotlin/Dictionary.kt
@@ -2,7 +2,7 @@ import java.util.*
 
 typealias Dictionary = Map<String, Postings>
 
-typealias MutableDictionary = MutableMap<String, MutablePostingsType>
+typealias MutableDictionary = MutableMap<String, MutablePostings>
 
 fun emptyDictionary(): Dictionary = emptyMap()
 
@@ -14,13 +14,13 @@ fun mutableDictionaryOf(): MutableDictionary = TreeMap()
 
 typealias Postings = Set<DocumentNumber>
 
-typealias MutablePostingsType = MutableSet<DocumentNumber>
+typealias MutablePostings = MutableSet<DocumentNumber>
 
 fun postingsOf(vararg documentNumber: String): Postings = mutableSetOf<DocumentNumber>().apply {
     addAll(documentNumber.map { DocumentNumber.parse(it) })
 }
 
-fun mutablePostingsOf(): MutablePostingsType = mutableSetOf()
+fun mutablePostingsOf(): MutablePostings = mutableSetOf()
 
 fun Dictionary.postingsCount() = entries.sumBy { (_, postings) -> postings.count() }
 
