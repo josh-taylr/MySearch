51fa56b74df3f81a30592e384272c1a76e5a8a9f
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-12-01 18:02:52 +1300

    Clear properties file

--- src/main/kotlin/constants.kt
+++ src/main/kotlin/constants.kt
@@ -1,2 +1,2 @@
-const val WSJ_FILE: String = "/Users/Josh/Documents/wsj.xml"
-const val INDEX_DIR: String = "/Users/Josh/.my_search"
+const val WSJ_FILE: String = ""
+const val INDEX_DIR: String = ""
