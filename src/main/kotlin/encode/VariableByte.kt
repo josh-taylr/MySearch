0e3684b67153284dd257d93c44392bfc4428b0d5
Josh Taylor <joshua.taylor.dev@gmail.com>
2018-12-02 23:39:49 +1300

    Refactor VariableByte

--- src/main/kotlin/encode/VariableByte.kt
+++ src/main/kotlin/encode/VariableByte.kt
@@ -1,34 +1,36 @@
 package encode
 
+@ExperimentalUnsignedTypes
 object VariableByte : NumberEncoder {
 
     private const val MAX_BYTE: UByte = 0b1000_0000u
 
     override fun decode(bytes: Iterable<Byte>): List<Long> {
         val numbers = mutableListOf<Long>()
-        var n = 0L
-        bytes.forEach { byte ->
-            val uByte = byte.toUByte()
-            if (uByte < MAX_BYTE) {
-                n = MAX_BYTE.toLong() * n + uByte.toLong()
-            } else {
-                n = MAX_BYTE.toLong() * n + (uByte xor MAX_BYTE).toLong()
-                numbers.add(n)
-                n = 0L
-            }
-        }
+        var n = 0uL
+        bytes.asSequence()
+                .map(Byte::toUByte)
+                .forEach { byte ->
+                    if (byte < MAX_BYTE) {
+                        n = MAX_BYTE * n + byte
+                    } else {
+                        n = MAX_BYTE * n + (byte xor MAX_BYTE)
+                        numbers.add(n.toLong())
+                        n = 0uL
+                    }
+                }
         return numbers
     }
 
     override fun encode(numbers: Iterable<Long>): List<Byte> = numbers.flatMap(this::encode)
 
     internal fun encode(number: Long): List<Byte> {
-        var num = number
+        var num = number.toULong()
         val bytes = mutableListOf<UByte>()
         do {
-            bytes += (num % MAX_BYTE.toLong()).toUByte()
-            num /= MAX_BYTE.toLong()
-        } while (num > 0)
+            bytes += (num % MAX_BYTE).toUByte()
+            num /= MAX_BYTE
+        } while (num > 0u)
         bytes[0] = bytes[0] or MAX_BYTE
         bytes.reverse()
         return bytes.map(UByte::toByte)
